3de0bd997698ba174a419ed1a08cbadbdc35a2fa
Semih <54319361+semihV23@users.noreply.github.com>
2021-05-20 10:30:01 +0300

    Add files via upload

---
+++ valorant.py
@@ -0,0 +1,250 @@
+from pynput.mouse import Button, Controller
+import time
+import keyboard
+
+#print("Starts after 10 seconds...")
+#time.sleep(10)
+print("started")
+
+mouse = Controller()
+while True:
+        if keyboard.is_pressed('1'):
+                while True:
+                        mouse.position = (668, 931)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('2'):
+                while True:
+                        mouse.position = (751, 927)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('3'):
+                while True:
+                        mouse.position = (837, 922)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('4'):
+                while True:
+                        mouse.position = (915, 930)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('5'):
+                while True:
+                        mouse.position = (996, 928)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('6'):
+                while True:
+                        mouse.position = (1086, 922)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('7'):
+                while True:
+                        mouse.position = (1165, 923)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('8'):
+                while True:
+                        mouse.position = (1248, 920)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('9'):
+                while True:
+                        mouse.position = (661, 1009)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('f2'):
+                while True:
+                        mouse.position = (745, 1011)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('f3'):
+                while True:
+                        mouse.position = (826, 1006)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('f4'):
+                while True:
+                        mouse.position = (914, 1010)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('f5'):
+                while True:
+                        mouse.position = (1009, 1011)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('f6'):
+                while True:
+                        mouse.position = (1093, 1010)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
+        if keyboard.is_pressed('f7'):
+                while True:
+                        mouse.position = (1176, 1009)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (745, 1015)")
+                        time.sleep(0.1)
+                        mouse.position = (926, 834)
+                        mouse.click(Button.left, 2)
+                        if keyboard.is_pressed('q'):  # if key 'q' is pressed 
+                                print('quit')
+                                break  # finishing the loop
+                        print("mouse.position = (926, 834)")
+                        time.sleep(0.1)
